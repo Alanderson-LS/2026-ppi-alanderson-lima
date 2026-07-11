9057939e11d595cd13d15885a0a607bbe0caddd3
Alanderson-LS <delimasoaresalanderson@gmail.com>
2026-07-11 17:16:36 -0300

    feat: create problem database schema
    Refs #2

---
+++ src/problemarium/schema.sql
@@ -0,0 +1,21 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS problem;
+
+CREATE TABLE user (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE problem (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author_id INTEGER NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    title TEXT NOT NULL,
+    statement TEXT NOT NULL,
+    subject TEXT NOT NULL,
+    difficulty TEXT NOT NULL,
+    answer TEXT NOT NULL,
+    resolution TEXT,
+    FOREIGN KEY (author_id) REFERENCES user (id)
+);
