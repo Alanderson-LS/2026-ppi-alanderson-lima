aef06a9c8ee9b9e55a013451d93f2a810f419307
Alanderson-LS <delimasoaresalanderson@gmail.com>
2026-07-12 16:17:57 -0300

    feat: implement problem update and delete, tested problem database
    Closes #2
    Closes #4

--- src/problemarium/problem.py
+++ src/problemarium/problem.py
@@ -46,3 +46,59 @@ def create():
             return redirect(url_for('problem.index'))
 
     return render_template('problem/create.html')
+
+def get_problem(id, check_author=True):
+    problem = get_db().execute(
+        'SELECT p.id, title, statement, created, author_id, username, subject, difficulty, answer, resolution'
+        ' FROM problem p JOIN user u ON p.author_id = u.id'
+        ' WHERE p.id = ?',
+        (id,)
+    ).fetchone()
+
+    if problem is None:
+        abort(404, f"Problem id {id} doesn't exist.")
+
+    if check_author and problem['author_id'] != g.user['id']:
+        abort(403)
+
+    return problem
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    problem = get_problem(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        statement = request.form['statement']
+        difficulty = request.form['difficulty']
+        answer = request.form['answer']
+        subject = request.form['subject']
+        resolution = request.form['resolution']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE problem SET title = ?, statement = ?, difficulty = ?, answer = ?, subject = ?, resolution = ?'
+                ' WHERE id = ?',
+                (title, statement, difficulty, answer, subject, resolution, id)
+            )
+            db.commit()
+            return redirect(url_for('problem.index'))
+
+    return render_template('problem/update.html', problem=problem)
+
+@bp.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_problem(id)
+    db = get_db()
+    db.execute('DELETE FROM problem WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('problem.index'))
