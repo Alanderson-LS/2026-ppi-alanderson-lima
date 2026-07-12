6459f30075139e59c9139b7d569f16a44fc17779
Alanderson-LS <delimasoaresalanderson@gmail.com>
2026-07-12 14:52:35 -0300

    feat: create problem page
    Refs #4

--- src/problemarium/auth.py
+++ src/problemarium/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from .db import get_db
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
@@ -62,3 +62,30 @@ def register():
         flash(error)
 
     return render_template('auth/register.html')
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT * FROM user WHERE id = ?', (user_id,)
+        ).fetchone()
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
--- src/problemarium/problem.py
+++ src/problemarium/problem.py
@@ -3,18 +3,46 @@ from flask import (
 )
 from werkzeug.exceptions import abort
 
-from flaskr.auth import login_required
-from flaskr.db import get_db
+from .auth import login_required
+from .db import get_db
 
 bp = Blueprint('problem', __name__)
 
 @bp.route('/')
 def index():
     db = get_db()
-    posts = db.execute(
-        'SELECT p.id, title, created, author_id, username'
+    problems = db.execute(
+        'SELECT p.id, title, created, author_id, username, statement, subject, difficulty, answer, resolution'
         ' FROM problem p JOIN user u ON p.author_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('problem/index.html', posts=posts)
+    return render_template('problem/index.html', problems=problems)
 
+@bp.route('/create', methods=('GET', 'POST'))
+@login_required
+def create():
+    if request.method == 'POST':
+        title = request.form['title']
+        statement = request.form['statement']
+        difficulty = request.form['difficulty']
+        answer = request.form['answer']
+        subject = request.form['subject']
+        resolution = request.form['resolution']
+        error = None
+
+        if not title:
+            error = 'Title is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'INSERT INTO problem (title, statement, author_id, difficulty, answer, subject, resolution)'
+                ' VALUES (?, ?, ?, ?, ?, ?, ?)',
+                (title, statement, g.user['id'], difficulty, answer, subject, resolution)
+            )
+            db.commit()
+            return redirect(url_for('problem.index'))
+
+    return render_template('problem/create.html')
